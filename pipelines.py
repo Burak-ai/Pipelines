3d1d185eeed8a3a160dc5684557e0d6a29ddd4e2
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-07-17 11:57:47 +0300

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -71,3 +71,84 @@ score = mean_absolute_error(y_valid, preds)
 print('MAE:', score)
 # Output:   MAE: 160679.18917034855
 
+# Improving the performance
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('train.csv', index_col='Id')
+X_test_full = pd.read_csv('test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# Break off validation set from training data
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X_full, y, 
+                                                                train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+categorical_cols = [cname for cname in X_train_full.columns if
+                    X_train_full[cname].nunique() < 10 and 
+                    X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if 
+                X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = categorical_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+X_test = X_test_full[my_cols].copy()
+print(X_train.head())
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Numerical preprocessing
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())
+])
+
+# Categorical preprocessing
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('encoder', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Bundle preprocessing
+preprocessor = ColumnTransformer(transformers=[
+    ('num', numerical_transformer, numerical_cols),
+    ('cat', categorical_transformer, categorical_cols)
+])
+
+# Model
+model = RandomForestRegressor(n_estimators=300, random_state=0)
+
+# Full pipeline
+clf = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', model)
+])
+
+# Fit model
+clf.fit(X_train, y_train)
+
+# Predict
+preds = clf.predict(X_valid)
+
+# Evaluate
+print("MAE:", mean_absolute_error(y_valid, preds))
+
+
